24d49fc9ec55e8014c828e0ecbcd4eb925f23a0b
Buğra Yılmaz <bryl.bugra@gmail.com>
2024-01-30 17:53:33 +0300

    Vehicle Fully Working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,19 @@
 const canv= document.querySelector("#canv");
 const ctx= canv.getContext("2d");
+let latestAnimationRequest;
 window.onload=()=>{
   canv.height=window.innerHeight;
   canv.width=window.innerWidth;
-  const carF = new Car(100,200,canv.width,canv.height,"white",100,100,ctx);
+  const carF = new Car(100,200,canv.width,canv.height,"white",500,500,ctx);
+  carF.animate();
 } 
+window.addEventListener("resize",()=>{
+  cancelAnimationFrame(latestAnimationRequest);
+  canv.height=window.innerHeight;
+  canv.width=window.innerWidth;
+  const carF = new Car(100,200,canv.width,canv.height,"white",500,500,ctx);
+  carF.animate();
+});
 
 class Car{
   #carWidth;
@@ -15,6 +24,7 @@ class Car{
   #carStartPosX;
   #carStartPosY;
   #ctx;
+  keyControls
   constructor(crWidth,crHeight,caWidth,caHeight,carColor,carPosX,carPosY,ctx){
     this.#ctx=ctx;
     this.#carWidth=crWidth;
@@ -25,17 +35,120 @@ class Car{
     this.#carStartPosY=carPosY;
     this.#ctx.fillStyle=carColor;
     this.#ctx.strokeStyle=carColor;
-    this.#draw();
-  }
+    this.keyControls = new keyControlHandler();
+
+    this.speed=0;
+    this.acceleration=2;
+    this.friction = 0.5;
+    this.maxSpeed=30;
+    this.angle=0;
+    }
+  
   #draw(){
+    this.#ctx.save();
+    this.#ctx.translate(this.#carStartPosX+this.#carWidth/2,this.#carStartPosY-this.#carHeight/2);
+    this.#ctx.rotate(this.angle);
     this.#ctx.beginPath();
-    this.#ctx.moveTo(this.#carStartPosX,this.#carStartPosY);
-    this.#ctx.lineTo(this.#carStartPosX+this.#carWidth,this.#carStartPosY);
-    this.#ctx.lineTo(this.#carStartPosX+this.#carWidth,this.#carStartPosY+this.#carHeight);
-    this.#ctx.lineTo(this.#carStartPosX,this.#carStartPosY+this.#carHeight);
-    this.#ctx.lineTo(this.#carStartPosX,this.#carStartPosY);
+    this.#ctx.moveTo(-this.#carWidth / 2, -this.#carHeight / 2);
+    this.#ctx.lineTo(this.#carWidth / 2, -this.#carHeight / 2);
+    this.#ctx.lineTo(this.#carWidth / 2, this.#carHeight / 2);
+    this.#ctx.lineTo(-this.#carWidth / 2, this.#carHeight / 2);
+    this.#ctx.lineTo(-this.#carWidth / 2, -this.#carHeight / 2);
     this.#ctx.closePath();
     this.#ctx.fill();
+    this.#ctx.restore();
   }
+  animate(){
+    this.#ctx.clearRect(0,0,this.#canvasWidth,this.#canvasHeight);
+    if(this.keyControls.forward){
+      this.speed+=this.acceleration;
+    }
+    if(this.keyControls.reverse){
+      this.speed-=this.acceleration;
+    }
+  
+    if(this.speed > this.maxSpeed){
+      this.speed=this.maxSpeed;
+    }
+    if(this.speed < -this.maxSpeed/3){
+      this.speed=-this.maxSpeed/3
+    }
+    if(this.speed>0){
+      this.speed-=this.friction;
+    }
+    else if(this.speed<0){
+      this.speed += this.friction;
+    }
+    if(this.keyControls.right){
+      this.angle+=0.1;
+    }
+    if(this.keyControls.left){
+      this.angle-=0.1;
+    }
+    this.#carStartPosY-=this.speed*Math.cos(this.angle);
+    this.#carStartPosX+=this.speed*Math.sin(this.angle);
+    this.#draw();
+    latestAnimationRequest=requestAnimationFrame(this.animate.bind(this));
+  }
+}
 
+
+class keyControlHandler{
+  constructor(){
+    this.forward=false;
+    this.reverse=false;
+    this.right=false;
+    this.left=false;
+    this.#keyListener();
+  }
+  #keyListener(){
+    window.addEventListener("keydown",(eventName)=>{
+      switch (eventName.key) {
+        case "ArrowUp":
+        case "w":
+        case "W":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+        case "s":
+        case "S":
+          this.reverse = true;
+          break;
+        case "ArrowLeft":
+        case "a":
+        case "A":
+          this.left = true;
+          break;
+        case "ArrowRight":
+        case "d":
+        case "D":
+          this.right = true;
+          break;
+      }
+    });
+    window.addEventListener("keyup",(eventName)=>{
+      switch (eventName.key) {
+        case "ArrowUp":
+        case "w":
+        case "W":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+        case "s":
+        case "S":
+          this.reverse = false;
+          break;
+        case "ArrowLeft":
+        case "a":
+        case "A":
+          this.left = false;
+          break;
+        case "ArrowRight":
+        case "d":
+        case "D":
+          this.right = false;
+          break;
+      }      
+    });
+  }
 }
